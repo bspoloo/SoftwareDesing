41068a7103752a91db2ad0ff70c91f41b8944aeb
Bryan Simón Polo Orellana <102623208+bspoloo@users.noreply.github.com>
2024-02-28 10:18:03 -0400

    changes added

--- src/main/java/bspo/SoftwareDesing/Exercise3Corrected/Commands/ICommand.java
+++ src/main/java/bspo/SoftwareDesing/Exercise3Corrected/Commands/ICommand.java
@@ -1,7 +1,5 @@
 package bspo.SoftwareDesing.Exercise3Corrected.Commands;
 
-import bspo.SoftwareDesing.Exercise3Corrected.IPagination;
-
 public interface ICommand<T> {
-    void execute(IPagination<T> pagination);
+    void execute(IPaginator<T> paginator);
 }
